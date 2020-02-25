81be836bf971668179ecd429b9d8f27b9def2058
straga <vostraga@gmail.com>
2020-02-25 10:26:37 +0200

    MQTT

--- pc_dev/gen.py
+++ pc_dev/gen.py
@@ -32,18 +32,17 @@ print("-- work_path: {}".format(work_root))
 print("-- Start Gen Lib")
 
 
-SYMLINK = False
+SYMLINK = True
 
 gen_libs = Gen(SYMLINK).gen_libs
 
-# main PC
-gen_libs(src_lib=path_platfrorm, src_path="pc", src="main.py", dst_lib=work_root, dst_path="")
+# main
+gen_libs(src_lib=path_platfrorm, src_path="esp32", src="main.py", dst_lib=work_root, dst_path="")
 
 
 # Platform PC
-gen_libs(src_lib=path_core, src_path="_board/pc", src="platform.py", dst_lib=work_core_path, dst_path="")
-gen_libs(src_lib=path_core, src_path="_board/pc", src="u_os.py", dst_lib=work_core_path, dst_path="")
-
+gen_libs(src_lib=path_core, src_path="_board/esp32", src="platform.py", dst_lib=work_core_path, dst_path="")
+gen_libs(src_lib=path_core, src_path="_board/esp32", src="u_os.py", dst_lib=work_core_path, dst_path="")
 
 # core
 gen_libs(src_lib=path_core, src_path="asyn", src="asyn.py", dst_lib=work_core_path, dst_path="asyn")
@@ -56,21 +55,27 @@ gen_libs(src_lib=path_core, src_path="mbus", src="mbus.py", dst_lib=work_core_pa
 gen_libs(src_lib=path_core, src_path="loader", src="loader.py", dst_lib=work_core_path, dst_path="loader")
 gen_libs(src_lib=path_core, src_path="loader", src="board_mod.json", dst_lib=work_core_path, dst_path="loader")
 
+
+
 # lib
 ## logging
 gen_libs(src_lib=path_app_lib, src_path="", src="logging.py", dst_lib=work_lib_path, dst_path="")
-## HW EMU
-gen_libs(src_lib=path_app_lib, src_path="esp", src="", dst_lib=work_lib_path, dst_path="esp")
-gen_libs(src_lib=path_app_lib, src_path="esp32", src="", dst_lib=work_lib_path, dst_path="esp32")
-gen_libs(src_lib=path_app_lib, src_path="machine", src="", dst_lib=work_lib_path, dst_path="machine")
-gen_libs(src_lib=path_app_lib, src_path="network", src="", dst_lib=work_lib_path, dst_path="network")
 
+## uasyncio
+gen_libs(src_lib=path_ulib, src_path="uasyncio/uasyncio", src="__init__.py", dst_lib=work_lib_path, dst_path="uasyncio")
+gen_libs(src_lib=path_ulib, src_path="uasyncio.core/uasyncio", src="core.py", dst_lib=work_lib_path, dst_path="uasyncio")
+gen_libs(src_lib=path_ulib, src_path="uasyncio.queues/uasyncio", src="queues.py", dst_lib=work_lib_path, dst_path="uasyncio")
+
+## collections
+gen_libs(src_lib=path_ulib, src_path="collections.deque/collections", src="deque.py", dst_lib=work_lib_path, dst_path="collections")
 
 # MOD
 gen_libs(src_lib=path_app_mod, src_path="board", src="", dst_lib=work_mod_path, dst_path="board")
 gen_libs(src_lib=path_app_mod, src_path="net", src="", dst_lib=work_mod_path, dst_path="net")
 gen_libs(src_lib=path_app_mod, src_path="ftp", src="", dst_lib=work_mod_path, dst_path="ftp")
 gen_libs(src_lib=path_app_mod, src_path="telnet", src="", dst_lib=work_mod_path, dst_path="telnet")
+gen_libs(src_lib=path_app_mod, src_path="mqtt", src="", dst_lib=work_mod_path, dst_path="mqtt")
+
 
 
 #     #PC
--- pc_dev/main.py
+++ pc_dev/main.py
@@ -1,125 +1,100 @@
 
-import sys
+import uasyncio as asyncio
+import machine, _thread
+import uos
+import gc
 
-part_name = "."
+# WDT
+async def run_wdt():
+    wdt = machine.WDT(timeout=120000)
+    print("WDT RUN")
+    while True:
+        wdt.feed()
+        gc.collect()
+        # print("WDT RESET")
+        await asyncio.sleep(10)
 
-sys.path.append("{}/".format(part_name))
-sys.path.append("{}/{}".format(part_name, "lib"))
-sys.path.append("{}/{}".format(part_name, "app"))
+# Lloader
+async def loader():
 
-import _thread
-import asyncio
+    # VFS SIZE
+    fs_stat = uos.statvfs('/')
+    fs_size = fs_stat[0] * fs_stat[2]
+    fs_free = fs_stat[0] * fs_stat[3]
+    print("File System Size {:,} - Free Space {:,}".format(fs_size, fs_free))
 
-from core.mbus.mbus import MbusManager
-from core.config.config import ConfigManager
-
-import logging
-
-log = logging.getLogger('MAIN')
-logging.basicConfig(level=logging.DEBUG)
-
-
-# from core.umod.config import uTable
-#
-# class MAIN(uTable):
-#
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self.part_name = "."
-#         self.core.mbus.sub_h(uid="MAIN", topic="ALL", env=self.env, func="print_mbus")
-#
-#     def print_mbus(self, *msg):
-#         print("MAIN: {}".format(msg))
-#
-# g_core.env["main"] = MAIN(tb_name="cfg_main", env="main", core=g_core)
-
-
-async def loader(core_mbus, core_conf):
-
-    from core.loader.loader import uCore
-    from core.loader.loader import uModule
-
-    log.info("CORE: init")
-    global g_core
-    g_core = uCore(core_mbus, core_conf)
-    g_core.part_name = part_name
-
-    log.info("Module: Init")
-    _umod = uModule(g_core)
-
-    log.info("Module: List")
-    await _umod.module_list()
-
-    log.info("Module: Schema")
-    await _umod.module_schema()
-
-    log.info("Module: Data")
-    await _umod.module_data()
-
-    log.info("Module: Act")
-    await _umod.module_act()
-
-
-def main():
-
-    # AsyncIO
-    loop = asyncio.get_event_loop()
-    _ = _thread.stack_size(100 * 1024)
+    part_name = uos.getcwd()
+    print(part_name)
+    #Import
+    from core.mbus.mbus import MbusManager
+    from core.config.config import ConfigManager
 
     # MBUS
-    log.info("MBUS START")
+    print("MBUS START")
     _mbus = MbusManager()
     _mbus.start()
 
     # CONF
-    log.info("CONF START")
+    print("CONF START")
     _conf = ConfigManager("./u_config")
 
+
+    # Core/Modules
+    from core.loader.loader import uCore
+    from core.loader.loader import uModule
+
+    print("CORE: init")
+    global g_core
+    g_core = uCore(_mbus, _conf)
+    g_core.part_name = part_name
+
+    print("Module: Init")
+    _umod = uModule(g_core)
+
+    print("Module: List")
+    await _umod.module_list()
+
+    print("Module: Schema")
+    await _umod.module_schema()
+
+    print("Module: Data")
+    await _umod.module_data()
+
+    print("Module: Act")
+    await _umod.module_act()
+
+
+
+
+def main():
+
+    loop = asyncio.get_event_loop()
+    _ = _thread.stack_size(8 * 1024)
     _thread.start_new_thread(loop.run_forever, ())
 
     # loop.set_debug(True)
-    loop.create_task(loader(_mbus, _conf))
 
-
-def clean_emu():
-
-    import os
-    from pathlib import Path
-    folder = './u_emu'
-
-    _folder = Path(folder)
-    if not _folder.exists():
-        _folder.mkdir()
-
-
-    for the_file in os.listdir(folder):
-        file_path = os.path.join(folder, the_file)
-        try:
-            if os.path.isfile(file_path):
-                os.unlink(file_path)
-        except Exception as e:
-            print(e)
+    loop.create_task(run_wdt())
+    loop.create_task(loader())
 
 
 
 if __name__ == '__main__':
-    log.info("MAIN")
 
-    log = logging.getLogger("FJSON")
-    log.setLevel(logging.DEBUG)
-
-
-    log = logging.getLogger("CONF")
-    log.setLevel(logging.DEBUG)
-
-
-    log = logging.getLogger("LOADER")
-    log.setLevel(logging.DEBUG)
-
-    log = logging.getLogger("MBUS")
-    log.setLevel(logging.DEBUG)
-
-
-    clean_emu()
+    print("MAIN")
     main()
+
+
+    #if error - run manualy in UART:
+    # import network
+    # sta = network.WLAN(network.STA_IF)
+    # sta.active(True)
+    # sta.connect("ssid", "psswd")
+    #
+    # import ftp
+    # ftp.ftpserver()
+
+
+
+
 
